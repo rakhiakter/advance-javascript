31391a709ac530971c1ceea79ad3f5e7cd7b4656
Rakhi Akter <67531354+rakhiakter@users.noreply.github.com>
2020-08-07 12:46:04 +0600

    null vs undefined clear

---
+++ null-vs-undefined.js
@@ -0,0 +1,18 @@
+let pakhi;
+console.log(pakhi);
+function add(num1 , num2){
+console.log(num1 + num2);
+}
+const result = add(13,34);
+console.log(result);
+function add(num1, num2){
+    console.log(num1, num2);
+}
+
+const result = add(13);
+console.log(result);
+let fun = undefined;
+console.log(fun);
+
+let ages = [2, 5, 9] 
+console.log(ages[11]);
