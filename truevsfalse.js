d959119c364ebf93df900c5b4bd7dfdf2db1fe7a
Rakhi Akter <67531354+rakhiakter@users.noreply.github.com>
2020-08-07 11:53:29 +0600

    true vs false

---
+++ truevsfalse.js
@@ -0,0 +1,21 @@
+// const age = 0;
+// const age = 4;
+// const age = -8;
+// falsy
+// ""
+// undefined
+// null
+// NaN
+// 0
+// truthy
+// '0',' '," ",[]
+
+
+
+const name = "Solaiman";
+if(name.length){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
